ce6b515bd93424cf73fbcb5c15ee291e7e8c0d84
Sadira <126765025+SadiraAstikl@users.noreply.github.com>
2023-10-26 15:22:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ document.getElementById('openModal').addEventListener("click", () => {
   document.getElementById('my-modal').classList.add('open')
 })
 
-
+document.getElementById('closeModal').addEventListener('click', () => {
+  document.getElementById('my-modal').classList.remove('open')
+})
 
